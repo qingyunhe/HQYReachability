0aaca5900d7ec12b78fbe5d07577e6c1adc583a0
qingyunhe <heqingyun1989@163.com>
2016-07-08 22:24:42 +0800

    修改

--- HQYViewController/HQYViewController/HQYTabBarController.m
+++ HQYViewController/HQYViewController/HQYTabBarController.m
@@ -39,6 +39,8 @@
     NSInteger count = self.childViewControllers.count;
     
     
+    
+    
     CGFloat x = 0;
     CGFloat y = 0;
     CGFloat w = [UIScreen mainScreen].bounds.size.width / count;
